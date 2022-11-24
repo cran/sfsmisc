4a200ab463037fd6ea161939b460503832fbbfa3
Martin Maechler <maechler@stat.math.ethz.ch>
2022-11-24 13:30:02 +0000

    version 1.1-14

--- R/misc-goodies.R
+++ R/misc-goodies.R
@@ -13,6 +13,9 @@
 ### ___Note___ we have some of these headers __MESS__
 ### But we leave it because of RCS {rather dismantle everything into 4-6 pieces
 
+## A fast substitute for structure() *when* we don't need to special case for factor, .Dim, ...
+struct <- function(x, ...) `attributes<-`(x, c(attributes(x), list(...)))
+
 ##-#### Vector, Matrix (or higher Array) stuff ########
 ##-###  -------------------------------------- ########
 
@@ -564,8 +567,8 @@ rDatetime <- function(n, min = "1900-01-01", max = "2100-12-31") {
         max <- as.POSIXct(max)
     else stop("'max' must be string (coercable to \"POSIXct\") or \"POSIXt\" object")
     stopifnot(length(min) == 1, length(max) == 1)
-    structure(runif(n, as.numeric(min), as.numeric(max)),
-              class = c("POSIXct", "POSIXt"), tzone = "")
+    struct(runif(n, as.numeric(min), as.numeric(max)),
+           class = c("POSIXct", "POSIXt"), tzone = "")
 }
 
 ###
@@ -1022,7 +1025,7 @@ prt.DEBUG <- function(..., LEVEL = 1) {
 ## --> ../man/read.org.table.Rd
 read.org.table <- function(file, header = TRUE, skip = 0,
                            encoding = "native", fileEncoding = "",
-                           text, ...) {
+                           text, quiet=FALSE, ...) {
     ## file - text   handling --- cut'n'paste from read.table()'s header
     if (missing(file) && !missing(text)) {
 	file <- textConnection(text, encoding = "UTF-8")
@@ -1058,8 +1061,18 @@ read.org.table <- function(file, header = TRUE, skip = 0,
     }
     ## drop empty lines at end only
     while(grepl("^ *$", tail(ll, 1L))) ll <- ll[-length(ll)]
+    nent <- count.fields(textConnection(ll), sep="|")
+    ok <- nent == (p <- length(col.names))
+    nrows <- if(all(ok)) length(ok)
+             else match(FALSE, ok) - 1L # 1..n, where n+1 contains the first non-match
+    if("nrows" %in% ...names())
+        nrows <- min(nrows, list(...)$nrows)
+    else if(!quiet && nrows < 0.95*length(nent))
+        message(
+            sprintf("Will read the first %d rows (of each %d columns) of %d (non-empty non-header) lines",
+                    nrows, p, length(nent)))
     f.ll <- textConnection(ll)# , encoding = "UTF-8"  is *NOT* good
     on.exit(close(f.ll))
     read.table(f.ll, header=FALSE, sep = "|",
-               col.names = col.names, ...) # , encoding = "UTF-8" *not* good
+               col.names = col.names, nrows=nrows, ...) # , encoding = "UTF-8" *not* good
 }
